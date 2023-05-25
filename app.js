87405a906e05df54846d7759bb49ffa452a250b2
radoneje <53825282+radoneje@users.noreply.github.com>
2023-05-25 15:03:06 +0300

    hhhhjj

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require('express')
 const path=require('path')
 const app = express()
 const port = 3000
+var knex = require('knex')({
+    client: 'mssql',
+    connection: {
+        server : 'localhost',
+        user : 'NewsFactoryUser',
+        password : 'ib',
+        database : 'NewsFactory'
+    }
+});
 
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Hey', message: 'Hello there!' })
+app.get('/', async (req, res) => {
+   let r= knex("Programs").where( {deleted:false}).where("id",">","0")
+    res.render('index', { title: 'Hey', message: 'Hello there!', prog:r })
 })
 
 app.listen(port, () => {
