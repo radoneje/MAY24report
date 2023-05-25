32095c112ca3ccf05f9d194f95c90b29276c1d27
radoneje <53825282+radoneje@users.noreply.github.com>
2023-05-25 14:52:44 +0300

    hhhhjj

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
 app.listen(port, () => {
