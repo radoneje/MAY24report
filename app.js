82ba10c5c73075a7f36de4493f465b4e2b7ad5ec
radoneje <53825282+radoneje@users.noreply.github.com>
2023-05-25 14:54:48 +0300

    hhhhjj

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const path=require('path')
 const app = express()
 const port = 3000
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug')
